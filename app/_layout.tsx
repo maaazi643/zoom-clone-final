3d57f16cda9299fab78951b46652f731cfe8d9fb
maaazi643 <adeyemimuaz101@gmail.com>
2024-10-31 08:01:06 +0100

    initial commit with clerk auth added

---
+++ app/_layout.tsx
@@ -0,0 +1,23 @@
+import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
+import { Slot } from "expo-router";
+
+import "react-native-reanimated";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error(
+    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+  );
+}
+
+export default function RootLayout() {
+  return;
+  {
+    <ClerkProvider publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <Slot />
+      </ClerkLoaded>
+    </ClerkProvider>;
+  }
+}
---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function IndexScreen() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
