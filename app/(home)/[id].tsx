e0047030281c71ab0375f232b77c50cac1a99f39
maaazi643 <adeyemimuaz101@gmail.com>
2024-11-01 17:57:02 +0100

    setting get stream

--- app/(home)/[id].tsx
+++ app/(home)/[id].tsx
@@ -1,10 +1,41 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  CallingState,
+  StreamCall,
+  useStreamVideoClient,
+} from "@stream-io/video-react-native-sdk";
+import { useLocalSearchParams } from "expo-router";
 
 export default function CallScreen() {
+  const [call, setCall] = useState<Call | null>(null);
+  const client = useStreamVideoClient();
+  const { id } = useLocalSearchParams();
+
+  useEffect(() => {
+    //cleanup functions
+    let slug: string;
+
+    if (id !== "(call)" && id) {
+      // Joining an existing call
+    } else {
+      // creating a new call
+      const _call = client?.call("default", "demo room");
+      _call?.join({ create: true }).then(() => {
+        //have a toast popup
+      });
+    }
+  }, [id, client]);
+  useEffect(() => {
+    //cleanup functions
+
+    if (call?.state.callingState !== CallingState.LEFT) {
+      call?.leave();
+    }
+  }, [call]);
   return (
-    <View>
+    <StreamCall call={call}>
       <Text>CallScreen</Text>
-    </View>
+    </StreamCall>
   );
 }
